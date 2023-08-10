b8e98263c8df6599d79d4cd969a443e745498e29
Felipe Duarte <feduarte.dev@gmail.com>
2023-08-10 14:58:57 -0300

    quarto requisito pronto

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 4', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found.', () => {
+    renderWithRouter(<App />, { route: '/teste' });
+    const notFoundText = screen.getByRole('heading', {
+      name: /page requested not found/i, level: 2,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem com o texto alternativo', () => {
+    renderWithRouter(<App />, { route: '/teste' });
+    const notFoundImgAltText = screen.getByAltText(
+      /clefairy pushing buttons randomly with text i have no idea what i'm doing/i,
+    );
+    expect(notFoundImgAltText).toBeInTheDocument();
+  });
+});
