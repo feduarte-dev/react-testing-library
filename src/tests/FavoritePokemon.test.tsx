847f35e6c4ad1c4305879f80622a3e13a8c2b5c6
Felipe Duarte <feduarte.dev@gmail.com>
2023-08-10 14:54:12 -0300

    terceiro requisito pronto

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 3', () => {
+  it('É exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito.', () => {
+    renderWithRouter(<App />, { route: '/favorites' });
+    const favText = screen.getByText(/no favorite pokémon found/i);
+    expect(favText).toBeInTheDocument();
+  });
+
+  it('É exibida na tela a mensagem No favorite pokemon found caso a pessoa não tenha Pokémon favorito.', async () => {
+    const { user } = renderWithRouter(<App />, { route: '/pokemon/4' });
+    const favCheckbox = screen.getByRole('checkbox', {
+      name: /pokémon favoritado\?/i,
+    });
+    await user.click(favCheckbox);
+    const favLink = screen.getByRole('link', {
+      name: /favorite pokémon/i,
+    });
+    await user.click(favLink);
+    const favPokemon = screen.getByText(/charmander/i);
+    expect(favPokemon).toBeInTheDocument();
+  });
+});
